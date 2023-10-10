76ec073697bc7676dcb214fe41471e25eeda61e9
Laurent Demailly <ldemailly@gmail.com>
2023-10-09 17:40:53 -0700

    Additional/unknown flags in configmaps/directories are warnings only now. (#45)
    * Additional/unknown flags are warnings only now. Fixes #44
    
    * deal with thread safety
    
    * go get -u ./...

--- configmap/updater.go
+++ configmap/updater.go
@@ -12,6 +12,7 @@ import (
 	"os"
 	"path"
 	"strings"
+	"sync/atomic"
 
 	"fortio.org/dflag"
 	"fortio.org/dflag/dynloglevel"
@@ -38,6 +39,7 @@ type Updater struct {
 	watcher    *fsnotify.Watcher
 	flagSet    *flag.FlagSet
 	done       chan bool
+	warnings   atomic.Int32 // Count of unknown flags that have been logged (increases at each iteration).
 }
 
 // Setup is a combination/shortcut for New+Initialize+Start.
@@ -124,9 +126,13 @@ func (u *Updater) readAll(dynamicOnly bool) error {
 			continue
 		}
 		fullPath := path.Join(u.dirPath, f.Name())
+		log.S(log.Debug, "checking flag", log.Str("flag", f.Name()), log.Str("path", fullPath))
 		if err := u.readFlagFile(fullPath, dynamicOnly); err != nil {
 			if errors.Is(err, errFlagNotDynamic) && dynamicOnly {
 				// ignore
+			} else if errors.Is(err, errFlagNotFound) {
+				log.S(log.Warning, "config map for unknown flag", log.Str("flag", f.Name()), log.Str("path", fullPath))
+				u.warnings.Add(1)
 			} else {
 				errorStrings = append(errorStrings, fmt.Sprintf("flag %v: %v", f.Name(), err.Error()))
 			}
@@ -137,6 +143,11 @@ func (u *Updater) readAll(dynamicOnly bool) error {
 			len(errorStrings), strings.Join(errorStrings, "\n"))
 	}
 	return nil
+}
+
+// Return the warnings count.
+func (u *Updater) Warnings() int {
+	return int(u.warnings.Load())
 }
 
 func (u *Updater) readFlagFile(fullPath string, dynamicOnly bool) error {
--- go.mod
+++ go.mod
@@ -7,7 +7,7 @@ require (
 	fortio.org/log v1.11.0
 	fortio.org/sets v1.0.3
 	github.com/fsnotify/fsnotify v1.6.0
-	golang.org/x/exp v0.0.0-20230321023759-10a507213a29
+	golang.org/x/exp v0.0.0-20231006140011-7918f672742d
 )
 
-require golang.org/x/sys v0.4.0 // indirect
+require golang.org/x/sys v0.13.0 // indirect
