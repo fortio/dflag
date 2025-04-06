a0ec694348f558a8b465ec212f7c36fb49f92370
Laurent Demailly <ldemailly@gmail.com>
2025-04-06 14:30:24 -0700

    Add more sleep which is quite bad to need in hope CI works again

--- configmap/loglevel_change_test.go
+++ configmap/loglevel_change_test.go
@@ -55,7 +55,7 @@ func TestDynamicLogLevelAndBinaryFlag(t *testing.T) {
 		t.Fatalf("unable to write %v: %v", binaryFlag, err)
 	}
 	// Time based tests aren't great, specially when ran on (slow) CI try to have notification not get events for above.
-	time.Sleep(3 * time.Second)
+	time.Sleep(5 * time.Second)
 	var u *configmap.Updater
 	log.SetLogLevel(log.Debug)
 	if u, err = configmap.Setup(flag.CommandLine, pDir); err != nil {
@@ -76,7 +76,7 @@ func TestDynamicLogLevelAndBinaryFlag(t *testing.T) {
 		t.Fatalf("unable to write %v: %v", fName, err)
 	}
 	// Time based tests aren't great, specially when ran on (slow) CI but...
-	time.Sleep(5 * time.Second)
+	time.Sleep(10 * time.Second)
 	newLevel := log.GetLogLevel()
 	if newLevel != log.Info {
 		t.Errorf("Loglevel didn't change as expected, still %v %v", newLevel, newLevel.String())
@@ -89,7 +89,7 @@ func TestDynamicLogLevelAndBinaryFlag(t *testing.T) {
 	if err = os.WriteFile(binaryFlag, []byte{1, 2, 3, 4, 5}, 0o644); err != nil {
 		t.Fatalf("unable to write %v: %v", binaryFlag, err)
 	}
-	time.Sleep(4 * time.Second)
+	time.Sleep(5 * time.Second)
 	// We might get more than 1 event for some reasons, so more than 1 error
 	errCount := u.Errors()
 	if errCount < 1 {
